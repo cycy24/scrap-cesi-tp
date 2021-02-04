2567bf1688f60ec824e285a18e32da6961a4c7ce
theo <t.samson@cobalt-informatique.com>
2021-02-04 12:02:22 +0100

    uiuiui la baguette

--- bot_discord/src/main.js
+++ bot_discord/src/main.js
@@ -1,4 +1,5 @@
 require('dotenv').config({path: __dirname + '/../.env'});
+const axios = require('axios');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -7,9 +8,11 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+client.on('message', async msg => {
+    if (msg.content === '!cesi') {
+        const response = await axios.get(`http://localhost:3000`);
+        console.log(response.data[0][0]);
+        msg.reply(JSON.stringify(response.data[0][0]));
     }
 });
 console.log(process.env);
