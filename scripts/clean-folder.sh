46b7a8bbfccce4c5f547c0fb989f702ec3325eca
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2021-02-05 16:56:02 +0100

    Update clean-folder.sh

--- scripts/clean-folder.sh
+++ scripts/clean-folder.sh
@@ -1,5 +1,5 @@
 if [ -d "/home/ec2-user/scrap-cesi-tp" ]; then
     cd /home/ec2-user/scrap-cesi-tp
     docker-compose -f "$DOCKER_COMPOSE_YML_PATH" kill
-    cd .. && rm -rf ./scraper-bot
+    cd .. && rm -rf ./scraper-bot ./scraper-bot-devops ./scraper-bot-devops.tar.gzip ./scraper-bot-nodevops ./scraper-bot-nodevops.tar.gzip
 fi
